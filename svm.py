fac2024d23ecd59832fd0b34dacb2790f2f1330c
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2017-04-18 18:52:40 +0700

    Add SVM evaluation using test set and add remove similar instance using LCS

--- svm.py
+++ svm.py
@@ -1,6 +1,8 @@
 import csv
+import gc
 from os.path import dirname, join
 from random import shuffle
+import resource
 import time
 
 import numpy as np
@@ -10,25 +12,19 @@ from sklearn.svm import LinearSVC
 
 from modules.cleaner import clean
 
+## Initialize categories and count vectorizer
+gc.disable()
 categories = ['traffic', 'non_traffic']
 count_vect = CountVectorizer(preprocessor=clean)
 
-with open(join(dirname(__file__), 'result/generated_datasets/traffic.csv'), newline='\n') as csv_input:
+## Open training set
+with open(join(dirname(__file__), 'result/generated_datasets/lcs/0.4/traffic.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
     traffic_tweets = [line[0] for line in dataset]
 
-with open(join(dirname(__file__), 'result/generated_datasets/non_traffic.csv'), newline='\n') as csv_input:
+with open(join(dirname(__file__), 'result/generated_datasets/lcs/0.4/non_traffic.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
     non_traffic_tweets = [line[0] for line in dataset]
-
-with open(join(dirname(__file__), 'tweets_corpus/test_set_10000.csv'), newline='\n') as csv_input:
-    dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
-    dataset = [(line[0], line[1]) for line in dataset]
-    shuffle(dataset)
-    test = {
-        'data': [line[0] for line in dataset],
-        'target': [categories.index(line[1]) for line in dataset],
-    }
 
 tweets = {
     'data': traffic_tweets + non_traffic_tweets,
@@ -40,9 +36,25 @@ training_vectors = count_vect.fit_transform(tweets['data'])
 # print(training_vectors.shape)
 # print(len(tweets['target']))
 
-start_time = time.time()
 clf = LinearSVC(max_iter=10000).fit(training_vectors, tweets['target'])
-training_time = round(time.time() - start_time, 2)
+print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+
+## Measure training time average of 100
+start_time = time.clock()
+for i in range(100):
+    clf = LinearSVC(max_iter=10000).fit(training_vectors, tweets['target'])
+training_time = round(time.clock() - start_time, 2) / 100
+gc.enable()
+
+## Open Test set
+with open(join(dirname(__file__), 'tweets_corpus/test_set_10000.csv'), newline='\n') as csv_input:
+    dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
+    dataset = [(line[0], line[1]) for line in dataset]
+    shuffle(dataset)
+    test = {
+        'data': [line[0] for line in dataset],
+        'target': [categories.index(line[1]) for line in dataset],
+    }
 
 test_vectors = count_vect.transform(test['data'])
 
@@ -56,6 +68,5 @@ print('Accuracy: {}'.format(accuracy))
 print('Precision: {}'.format(prfs[0][0]))
 print('Recall: {}'.format(prfs[1][0]))
 print('F-score: {}'.format(prfs[2][0]))
-
 # for doc, category in zip(test_doc, predicted):
 #     print('%r => %s' % (doc, categories[category]))
