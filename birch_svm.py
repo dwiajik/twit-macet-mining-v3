f0e042b73773dd222df3c4da0a98c6fcacc319e0
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2017-04-20 19:50:21 +0700

    Make progress stdout

--- birch_svm.py
+++ birch_svm.py
@@ -10,8 +10,14 @@ from sklearn.svm import LinearSVC
 
 from modules.cleaner import clean
 
+print('PROGRESS: Initializing...')
+
 count_vect = CountVectorizer(preprocessor=clean)
 clf = LinearSVC(max_iter=10000)
+brc = Birch(branching_factor=50, n_clusters=None, threshold=2.5, compute_labels=True)
+splitBy = 20
+
+print('PROGRESS: Loading datasets...')
 
 with open(join(dirname(__file__), 'result/generated_datasets/traffic.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
@@ -21,40 +27,6 @@ with open(join(dirname(__file__), 'result/generated_datasets/non_traffic.csv'), 
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
     non_traffic_tweets = [line[0] for line in dataset]
 
-shuffle(traffic_tweets)
-shuffle(non_traffic_tweets)
-
-traffic_tweets_size = int(len(traffic_tweets) / 20)
-
-traffic_tweets = traffic_tweets[:traffic_tweets_size]
-non_traffic_tweets = non_traffic_tweets[:int(len(non_traffic_tweets) / 20)]
-
-vectors = count_vect.fit_transform(traffic_tweets + non_traffic_tweets)
-print(vectors.shape)
-
-traffic_vectors = vectors[:traffic_tweets_size]
-non_traffic_vectors = vectors[traffic_tweets_size:]
-print(traffic_vectors.shape)
-print(non_traffic_vectors.shape)
-
-brc = Birch(branching_factor=50, n_clusters=None, threshold=2.5, compute_labels=True)
-
-brc.fit(traffic_vectors)
-traffic_vectors = brc.subcluster_centers_
-print(len(traffic_vectors))
-
-brc.fit(non_traffic_vectors)
-non_traffic_vectors = brc.subcluster_centers_
-print(len(non_traffic_vectors))
-
-training_vectors = np.concatenate((traffic_vectors, non_traffic_vectors))
-training_target = [True] * len(traffic_vectors) + [False] * len(non_traffic_vectors)
-
-print(len(training_vectors))
-
-clf.fit(training_vectors, training_target)
-
-## Open Test set
 with open(join(dirname(__file__), 'tweets_corpus/test_set_10000.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
     dataset = [(line[0], line[1]) for line in dataset]
@@ -64,7 +36,53 @@ with open(join(dirname(__file__), 'tweets_corpus/test_set_10000.csv'), newline='
         'target': [line[1] == 'traffic' for line in dataset],
     }
 
+print('PROGRESS: Shuffling datasets...')
+
+shuffle(traffic_tweets)
+shuffle(non_traffic_tweets)
+
+print('PROGRESS: Reducing datasets by 1/{} ...'.format(splitBy))
+
+traffic_tweets_size = int(len(traffic_tweets) / splitBy)
+non_traffic_tweets_size = int(len(non_traffic_tweets) / splitBy)
+
+traffic_tweets = traffic_tweets[:traffic_tweets_size]
+non_traffic_tweets = non_traffic_tweets[:non_traffic_tweets_size]
+
+print('PROGRESS: Extracting features of datasets...')
+
+vectors = count_vect.fit_transform(traffic_tweets + non_traffic_tweets)
+print('\tAll {} data feature vector shape: {}'.format(traffic_tweets_size + non_traffic_tweets_size, vectors.shape))
+
+traffic_vectors = vectors[:traffic_tweets_size]
+non_traffic_vectors = vectors[traffic_tweets_size:]
+print('\tTraffic data feature vector shape: {}'.format(traffic_vectors.shape))
+print('\tNon traffic data feature vector shape: {}'.format(non_traffic_vectors.shape))
+
 test_vectors = count_vect.transform(test['data'])
+print('\tTest data feature vector shape: {}'.format(test_vectors.shape))
+
+print('PROGRESS: Clustering dataset...')
+
+brc.fit(traffic_vectors)
+traffic_vectors = brc.subcluster_centers_
+print('\tTraffic data centroids count: {}'.format(len(traffic_vectors)))
+
+brc.fit(non_traffic_vectors)
+non_traffic_vectors = brc.subcluster_centers_
+print('\tNon traffic data centroids count: {}'.format(len(non_traffic_vectors)))
+
+training_vectors = np.concatenate((traffic_vectors, non_traffic_vectors))
+training_target = [True] * len(traffic_vectors) + [False] * len(non_traffic_vectors)
+
+print('\tTotal centroid count: {}'.format(len(training_vectors)))
+
+print('PROGRESS: Train SVM with all the data...')
+
+target = [True] * len(traffic_tweets) + [False] * len(non_traffic_tweets)
+clf.fit(vectors, target)
+
+print('PROGRESS: Evaluate the SVM model using test set...')
 
 predicted = clf.predict(test_vectors)
 accuracy = np.mean(predicted == test['target'])
@@ -72,7 +90,24 @@ accuracy = np.mean(predicted == test['target'])
 prfs = precision_recall_fscore_support(test['target'], predicted)
 
 # print('Training time: {}'.format(training_time))
-print('Accuracy: {}'.format(accuracy))
-print('Precision: {}'.format(prfs[0][0]))
-print('Recall: {}'.format(prfs[1][0]))
-print('F-score: {}'.format(prfs[2][0]))
+print('\tAccuracy: {}'.format(accuracy))
+print('\tPrecision: {}'.format(prfs[0][0]))
+print('\tRecall: {}'.format(prfs[1][0]))
+print('\tF-score: {}'.format(prfs[2][0]))
+
+print('PROGRESS: Train SVM with cluster centroids...')
+
+clf.fit(training_vectors, training_target)
+
+print('PROGRESS: Evaluate the SVM model using test set...')
+
+predicted = clf.predict(test_vectors)
+accuracy = np.mean(predicted == test['target'])
+
+prfs = precision_recall_fscore_support(test['target'], predicted)
+
+# print('Training time: {}'.format(training_time))
+print('\tAccuracy: {}'.format(accuracy))
+print('\tPrecision: {}'.format(prfs[0][0]))
+print('\tRecall: {}'.format(prfs[1][0]))
+print('\tF-score: {}'.format(prfs[2][0]))
